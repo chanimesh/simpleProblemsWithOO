08994a3557ca2eca4edb6b0b383e9d31c271d044
chanimesh <animeshc@thoughtworks.com>
2016-04-17 15:01:15 +0530

    added gitignore

--- primenumber/tests/test_primeNumbers.py
+++ primenumber/tests/test_primeNumbers.py
@@ -4,26 +4,24 @@ from  primeNumbers.PrimeNumbers import PrimeNumbers
 
 class simpleTests(unittest.TestCase):
 
-    def testing_with_eight(self):
+    def __init__(self):
+        prime = PrimeNumbers()
+
+    def test_list_primenumberrs_with_non_prime_end(self):
         prime = PrimeNumbers()
         self.assertEqual(prime.list_primenumbers(8),[2,3,5,7])
 
-    def testing_with_11(self):
-        prime = PrimeNumbers()
-        self.assertEqual(prime.list_primenumbers(11),[2,3,5,7,11])
+    def test_list_primenumberrs_with_prime_end(self):
+        self.assertEqual(self.prime.list_primenumbers(11),[2,3,5,7,11])
 
-    def test_is_divisible_4_by_2(self):
-        prime= PrimeNumbers()
-        self.assertEqual(prime.is_divisible(2,4),True)
+    def test_is_divisible_with_zero_remainder(self):
+        self.assertEqual(self.prime.is_divisible(2,4),True)
 
-    def test_isdivisible_7_3(self):
-        prime=PrimeNumbers()
-        self.assertEqual(prime.is_divisible(3,7),False)
+    def test_is_divisible_with_non_zero_remainder(self):
+        self.assertEqual(self.prime.is_divisible(3,7),False)
 
-    def test_is_prime_7(self):
-        prime = PrimeNumbers()
-        self.assertEqual(prime.is_prime(7),True)
+    def test_is_prime_with_prime(self):
+        self.assertEqual(self.prime.is_prime(7),True)
 
-    def test_is_prime_10(self):
-        prime = PrimeNumbers()
-        self.assertEqual(prime.is_prime(70),False)
+    def test_is_prime_with_non_prime(self):
+        self.assertEqual(self.prime.is_prime(70),False)
