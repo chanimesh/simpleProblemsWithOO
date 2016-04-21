09fc1d8b03d3a500d5b0ba63c7dc07a468b30cc9
chanimesh <animeshc@thoughtworks.com>
2016-04-22 00:56:02 +0530

    completed reverse sentence

--- ReverseSentence/src/ReverseSentence.py
+++ ReverseSentence/src/ReverseSentence.py
@@ -1,11 +1,16 @@
 class ReverseSentence:
 
-    def splitSentence(self,sentence):
+    def __splitSentence(self,sentence):
         return sentence.split()
 
-    def reverse_list(self, stringList):
+    def __reverse_list(self, stringList):
         stringList.reverse()
         return stringList
 
-    def join_list(self,stringList):
-        return " ".join(stringList) 
+    def __join_list(self,stringList):
+        return " ".join(stringList)
+
+    def reverseSentence(self,sentence_string):
+        sentence_list = self.__splitSentence(sentence_string)
+        sentence_list = self.__reverse_list(sentence_list)
+        return self.__join_list(sentence_list)
--- ReverseSentence/tests/test_reverseSentence.py
+++ ReverseSentence/tests/test_reverseSentence.py
@@ -6,16 +6,26 @@ class TestReverseSentence(TestCase):
     def setUp(self):
         self.reverse = ReverseSentence()
 
-    def test_splitSentence_with_single_spaces(self):
-        self.assertEqual(self.reverse.splitSentence("this is a sentence"),["this","is","a","sentence"])
+    # def test_splitSentence_with_single_spaces(self):
+    #     self.assertEqual(self.reverse.splitSentence("this is a sentence"),["this","is","a","sentence"])
+    #
+    # def test_splitSentence_with_multiple_spaces(self):
+    #     self.assertEqual(self.reverse.splitSentence("this  is a sentence"), ["this", "is", "a", "sentence"])
+    #
+    # def test_reverseList(self):
+    #     self.assertEqual(self.reverse.reverse_list(["this", "is", "a", "sentence"]), ["sentence", "a", "is", "this"])
+    #
+    # def test_joinList(self):
+    #     self.assertEqual(self.reverse.join_list(["this", "is", "a", "sentence"]), "this is a sentence")
 
-    def test_splitSentence_with_multiple_spaces(self):
-        self.assertEqual(self.reverse.splitSentence("this  is a sentence"), ["this", "is", "a", "sentence"])
+    def test_reverseSentence_singlespaces(self):
+        self.assertEqual(self.reverse.reverseSentence("this is a sentence"),"sentence a is this")
 
-    def test_reverseList(self):
-        self.assertEqual(self.reverse.reverse_list(["this", "is", "a", "sentence"]), ["sentence", "a", "is", "this"])
+    def test_reverseSentence_multiplespaces(self):
+        self.assertEqual(self.reverse.reverseSentence("this   is a sentence"),"sentence a is this")
 
-    def test_joinList(self):
-        self.assertEqual(self.reverse.join_list(["this", "is", "a", "sentence"]), "this is a sentence")
+    def test_reverseSentence_spaces_begining(self):
+        self.assertEqual(self.reverse.reverseSentence(" this is a sentence"), "sentence a is this")
 
-    def test_
+    def test_reverseSentence_spaces_end(self):
+        self.assertEqual(self.reverse.reverseSentence("this is a sentence  "), "sentence a is this")
